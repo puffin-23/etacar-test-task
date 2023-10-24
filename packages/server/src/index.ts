63067965cfe38f8e73b85c7af4fcfd84b24a0ee4
Dasha Stashkevich <stash.dashawork@gmail.com>
2023-10-24 20:39:40 +0300

    feat(server): init express server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,32 @@
-import { initTRPC} from "@trpc/server"
+import { inferAsyncReturnType, initTRPC} from "@trpc/server"
+import { createExpressMiddleware, CreateExpressContextOptions } from '@trpc/server/adapters/express'
+import express from 'express'
 
-export const t = initTRPC.create()
+const createContext = ({
+   req,
+   res,}: CreateExpressContextOptions) => ({})
+ type Context = inferAsyncReturnType<typeof createContext>
+ 
+const t = initTRPC.context<Context>().create()
 
 const appRouter = t.router({
    ping:t.procedure.query(() => {
       return "pong"
    })
 })
+
+const app = express()
+
+app.use('/trpc', createExpressMiddleware({
+   router: appRouter,
+   createContext,
+}),
+)
+
+app.listen(4000, () => {
+   console.log('Server started.');
+})
+
+
+
+
